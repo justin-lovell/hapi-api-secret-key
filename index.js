e9b60fd4d49ca3ade935751f172a3472a69035ff
Justin Lovell <justin@justjuzzy.com>
2017-03-06 14:49:56 +1100

    abstract key fetching to an option passed function

--- index.js
+++ index.js
@@ -17,8 +17,16 @@ function loadApiKeysFromEnvironmentVariables() {
 }
 
 
+function createFetchUserApiKey() {
+  return function fetchUserSuppliedUserApiKey(request) {
+    return request.headers["api-key"] || request.query["api-key"];
+  }
+}
+
+
 function createInterceptor(options) {
   var secrets = (options && options.secrets) || loadApiKeysFromEnvironmentVariables();
+  var fetchUserSuppliedSecret = options.fetchUserApiKey || createFetchUserApiKey();
 
   return function (request, reply) {
     var tags = request.route.settings.tags;
@@ -31,7 +39,7 @@ function createInterceptor(options) {
       return reply.continue();
     }
 
-    var apiKey = request.headers["api-key"] || request.query["api-key"];
+    var apiKey = fetchUserSuppliedSecret(request);
     if (secrets && secrets.indexOf(apiKey) >= 0) {
       return reply.continue();
     }
